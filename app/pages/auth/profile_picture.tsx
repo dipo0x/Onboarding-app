2df7a480b0344e9d0b013e689b9a890bd4ca3a0a
Oladipo Adesiyan <adesiyanoladipo1@gmail.com>
2025-11-08 12:46:04 +0100

    ref: made minor update

--- app/pages/auth/profile_picture.tsx
+++ app/pages/auth/profile_picture.tsx
@@ -21,7 +21,7 @@ export default function ProfilePicture() {
   };
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images", "videos"],
+      mediaTypes: ["images"],
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1,
